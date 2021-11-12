9a377a94640557dd8b0202803b08df7787186a5c
WuhuFlying <360786367@qq.com>
2021-11-12 16:37:47 +0800

    feat:标记无人座位的座位号

--- algorithm/detect_seat.py
+++ algorithm/detect_seat.py
@@ -1,21 +1,16 @@
 import cv2.cv2 as cv2
 import os
+import json
 
-txt_array = []
-
-
-def get_seats_location():
-    txt_path = 'labels/study_room_1.txt'
-    with open(txt_path) as f:
-        for line in f:
-            str_list = line.split()
-            txt_array.append(str_list)
+all_seats = {}
 
 
-def get_reserved_seat(seats):  # 获取预约位置的坐标
+def get_reserved_seat(seats):  # 获取预约位置的坐标信息
     seats_location = []
     for i in range(len(seats)):
-        seats_location.append(txt_array[seats[i]])
+        if str(seats[i]) in all_seats.keys():
+            seat = all_seats[str(seats[i])]
+            seats_location.append([str(seats[i]), seat['x'], seat['y'], seat['width'], seat['height']])
 
     return seats_location
 
@@ -28,13 +23,13 @@ def get_coordinate(height, width, array, para):
         y = width * (float(array[i][1]))
         h = height * (float(array[i][4]))
         w = width * (float(array[i][3]))
-        xvar = w*para
-        yvar = h*para
+        xvar = w * para
+        yvar = h * para
         ymin = int(x - h / 2 + yvar)
         xmin = int(y - w / 2 + xvar)
         ymax = int(x + h / 2 - yvar)
         xmax = int(y + w / 2 - xvar)
-        coordinates.append([xmin, ymin, xmax, ymax])
+        coordinates.append([array[i][0], xmin, ymin, xmax, ymax])
     return coordinates
 
 
@@ -47,34 +42,38 @@ def detect(location):
     w = size[1]
     head_txt_path = '../runs/detect/exp/labels/studyroom.txt'
     head_array = []
-    with open(head_txt_path) as f:
-        for line in f:
+    with open(head_txt_path) as file:
+        for line in file:
             str_list = line.split()
             head_array.append(str_list)
-    head_array = get_coordinate(h, w, head_array, 0.7)
+    head_array = get_coordinate(h, w, head_array, 0.75)
     location = get_coordinate(h, w, location, 1.0)
 
     for i in range(len(location)):
         head_miss = True
         for j in range(len(head_array)):
-            if (head_array[j][0] >= location[i][0] and head_array[j][1] >= location[i][1]
-                     and head_array[j][2] <= location[i][2] and head_array[j][3] <= location[i][3]):
+            if (head_array[j][1] >= location[i][1] and head_array[j][2] >= location[i][2]
+                    and head_array[j][3] <= location[i][3] and head_array[j][4] <= location[i][4]):
                 head_miss = False
                 break
+        # cv2.rectangle(img, (location[i][1], location[i][2]), (location[i][3], location[i][4]), (0, 255, 0), 1)
+        # text = location[i][0]
+        # font = cv2.FONT_HERSHEY_SIMPLEX
+        # cv2.putText(img, text, (location[i][1], location[i][2]), font, 0.3, (0, 0, 255), 1)
         if head_miss:
-            cv2.rectangle(img, (location[i][0], location[i][1]), (location[i][2], location[i][3]), (0, 255, 0), 1)
-            # text = '{}'.format(i)  # 座位标号未实现
-            text = 'null'
+            cv2.rectangle(img, (location[i][1], location[i][2]), (location[i][3], location[i][4]), (0, 255, 0), 1)
+            text = location[i][0]
             font = cv2.FONT_HERSHEY_SIMPLEX
-            cv2.putText(img, text, (location[i][0], location[i][1]), font, 0.5, (0, 0, 255), 1)
+            cv2.putText(img, text, (location[i][1], location[i][2]), font, 0.3, (0, 0, 255), 1)
 
     cv2.imshow("test", img)
     cv2.waitKey(0)
 
 
 if __name__ == '__main__':
-    get_seats_location()
-    # reserved_seats = [49, 50, 51, 52, 53, 54, 55, 56, 57, 58]  # 预约的座位
-    reserved_seats = list(range(0, 83))
-    locations = get_reserved_seat(reserved_seats)  # 获取预约座位坐标
+    with open('seats.json', 'r') as f:
+        content = f.read()
+        all_seats = json.loads(content)
+    reserved_seats = list(range(1, 500))
+    locations = get_reserved_seat(reserved_seats)  # 获取预约位置的坐标信息
     detect(locations)  # 检测
